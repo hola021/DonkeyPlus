88f64c6af45b47e9d415066077926a36be08e149
SimPham02 <147654031+SimPham02@users.noreply.github.com>
2025-03-18 21:29:17 +0700

    Update random

--- src/main/java/sim/donkeyPlus/events/dupe.java
+++ src/main/java/sim/donkeyPlus/events/dupe.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import java.util.Random;
 
 public class dupe  implements  Listener {
     @EventHandler
@@ -14,9 +15,8 @@ public class dupe  implements  Listener {
 
         if((entity.getType() == EntityType.DONKEY || entity.getType() == EntityType.LLAMA)
                 && (!(entity instanceof LivingEntity livingEntity) || livingEntity.isInvisible())) {
-            double randomDouble = Math.random();
-            randomDouble *= 6.0D;
-            int randomInt = (int) randomDouble;
+            Random random = new Random();
+            int randomInt = random.nextInt(6);
             for (int i = 0; i < randomInt; i++) {
                 for (int x = 0; x < b.getDrops().size(); x++)
                     entity.getWorld().dropItemNaturally(entity.getLocation(), b.getDrops().get(x));
